83dcc80f9df038516d7c548ebfbfc8922905e002
wenhan.liu <dgs122062@gamil.com>
2025-01-28 14:44:58 -0700

    update github repo link

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,14 +2,10 @@ import StudentInfo from "@/app/week-2/student-info";
 
 export default function Page()
 {
-    const studentDict = {
-        "Wenhan Liu": "https://github.com/Hamabi-Minami?tab=repositories"
-    }
-
     return(
         <main>
             <h1>shopping List</h1>
-            <StudentInfo name={"Wenhan Liu"} githubLink={"https://github.com/Hamabi-Minami?tab=repositories"}></StudentInfo>
+            <StudentInfo name={"Wenhan Liu"} githubLink={"https://github.com/Hamabi-Minami/CRPG306-Assignments"}></StudentInfo>
         </main>
     )
 }
