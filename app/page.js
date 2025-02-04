07a77df9517b779eb1f92eee3b5f29e3d7f784e7
Hamabi_minami <1220625658@qq.com>
2025-02-04 15:31:56 -0700

    add style for assignment list

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ export default function Home() {
     return (
         <div className={"text-center"}>
             <h1 className={"text-2xl font-bold"}>CPRG306:Web Development 2 - Assignments</h1>
-            <a href={"/week-2"} className="text-blue-500 underline">Go to Week-2 Page</a>
-            <a href={"/week-3"} className="text-blue-500 underline">Go to Week-3 Page</a>
+            <ul>
+                <li className="list-unstyled">
+                    <a href={"/week-2"} className="text-blue-500 underline">Go to Week-2 Page</a>
+                </li>
+                <li className="list-unstyled">
+                    <a href={"/week-3"} className="text-blue-500 underline">Go to Week-3 Page</a>
+                </li>
+            </ul>
         </div>
     );
 }
