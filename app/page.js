413c3fc2206eb8abb00e2e06137ff8b907cce9d5
Hamabi_minami <1220625658@qq.com>
2025-02-04 15:30:26 -0700

    add router for "/week-3"

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-
 export default function Home() {
-  return (
-    <div className={"text-center"}>
-      <h1 className={"text-2xl font-bold"}>CPRG306:Web Development 2 - Assignments</h1>
-      <a href={"/week-2"} className="text-blue-500 underline">Go to Week-2 Page</a>
-    </div>
-  );
+    return (
+        <div className={"text-center"}>
+            <h1 className={"text-2xl font-bold"}>CPRG306:Web Development 2 - Assignments</h1>
+            <a href={"/week-2"} className="text-blue-500 underline">Go to Week-2 Page</a>
+            <a href={"/week-3"} className="text-blue-500 underline">Go to Week-3 Page</a>
+        </div>
+    );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
     return (
         <div>
-            <h1 className="text-4xl font-bold text-center mb-8 text-gray-800">Shopping List</h1>
+            <h1 className="text-4xl font-bold text-center mb-8 text-white-800">Shopping List</h1>
             <ItemList/>
         </div>)
 }
